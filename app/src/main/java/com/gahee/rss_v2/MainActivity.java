be99a323220e76ee82409ebf3dd7251360b9ab57
Gahee Han <leisure306@gmail.com>
2019-07-19 22:11:30 +0900

    Create Bottom navigation activity & fragments

--- app/src/main/java/com/gahee/rss_v2/MainActivity.java
+++ app/src/main/java/com/gahee/rss_v2/MainActivity.java
@@ -1,82 +1,55 @@
 package com.gahee.rss_v2;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
-import android.util.Log;
 
-import com.gahee.rss_v2.retrofit.NasaAPI;
-import com.gahee.rss_v2.retrofit.RetrofitInstanceBuilder;
-import com.gahee.rss_v2.retrofit.model.ArticleObj;
-import com.gahee.rss_v2.retrofit.model.ChannelObj;
-import com.gahee.rss_v2.retrofit.tags.Channel;
-import com.gahee.rss_v2.retrofit.tags.Image;
-import com.gahee.rss_v2.retrofit.tags.Item;
-import com.gahee.rss_v2.retrofit.tags.Rss;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-import java.util.ArrayList;
-import java.util.List;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final String TAG = "MainActivity";
-    private ArrayList<ChannelObj> mChannelObjArrayList = new ArrayList<>();
-    private ArrayList<ArticleObj> mArticleObjArrayList = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        BottomNavigationView navView = findViewById(R.id.nav_view);
 
+        navView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
 
-        NasaAPI nasaAPI = RetrofitInstanceBuilder.getNasaApi();
-        Call<Rss> call = nasaAPI.getBreakingNews();
-
-        call.enqueue(new Callback<Rss>() {
-            @Override
-            public void onResponse(Call<Rss> call, Response<Rss> response) {
-                if(response.body() != null && response.body().getChannel() != null){
-                    Channel channel = response.body().getChannel();
-                    String title = channel.getChannelTitle();
-                    String description = channel.getChannelDescription();
-                    String link = channel.getChannelLink();
-                    List<Item> listOfItems = channel.getItem();
-                    String imageUrl = channel.getImage() != null ? channel.getImage().getUrl() : null;
-
-                    Log.d(TAG, "channel : " + channel + "\n" + "title : " + title + "\n" + "description : " + description + "\n" + "Image : " + imageUrl + "\n");
-
-                    mChannelObjArrayList.add(new ChannelObj(title, description, link, listOfItems, imageUrl));
-                    for(int i = 0; i < listOfItems.size(); i++){
-                        Item item = channel.getItem().get(i);
-                        String articleTitle = item.getTitle();
-                        String articleLink = item.getLink();
-                        String articleDescription = item.getDescription();
-                        String articleMedia = item.getEnclosure().getUrl();
-                        String articlePubDate = item.getPubDate();
-                        String articleSource = item.getSource();
-
-                        Log.d(TAG, "article Link : " + articleLink + " \n" +
-                                "article Media : " + articleMedia + "\n" +
-                                "article PubDate : " + articlePubDate + "\n");
-                        mArticleObjArrayList.add(new ArticleObj(articleTitle, articleLink, articleDescription, articleMedia, articlePubDate, articleSource));
-                    }
-                }
-            }
-
-            @Override
-            public void onFailure(Call<Rss> call, Throwable t) {
-
-                Log.d(TAG, "failed to fetch data :  " + t.getMessage());
-
-            }
-        });
-
-
+        fragmentTransactionHelper(new HomeFragment());
     }
 
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    fragmentTransactionHelper(new HomeFragment());
+                    return true;
+                case R.id.navigation_dashboard:
+                    fragmentTransactionHelper(new FeedFragment());
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    private void fragmentTransactionHelper(final Fragment fragment)   {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.replace(R.id.fragment_placeholder, fragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
+    }
 
 }
